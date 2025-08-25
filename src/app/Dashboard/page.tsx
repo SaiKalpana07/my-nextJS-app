d8832928e97de497dfc6966e28927f1e0ffe5328
SaiKalpana Reddy <saireddy7991@gmail.com>
2025-08-25 17:12:44 +0530

    Dynamic routing

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
 
-export default function Dashboard() {
+import React from "react";
+import { useRouter } from "next/navigation";
+import Settings from "./Settings/page";
+import Link from "next/link";
+
+const Dashboard: React.FC = () => {
+  const router = useRouter();
+  const message: string = "Hello";
+
   return (
     <div>
       <p>Dashboard</p>
+      <button
+        onClick={() => {
+          router.push("/Dashboard/Settings");
+        }}
+      >
+        Click Next
+      </button>
+      <Link href={{ pathname: "/Dashboard/Settings", query: { id: "test" } }}>
+        Click this link
+      </Link>
+      <Settings />
     </div>
-  )
-}
+  );
+};
+
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function app() {
   return (
     <>
-    <p>Main file</p>
+      <p>Main file</p>
+      <div className="nav-links">
+        <Link className="links" href="/Home">Home</Link>
+        <Link className="links" href="/About">About</Link>
+        <Link className="links" href="/Dashboard">Dashboard</Link>
+        <Link className="links" href="/Products">Products</Link>
+        <Link className="links" href="/SignIn">SignIn</Link>
+      </div>
     </>
   );
 }
